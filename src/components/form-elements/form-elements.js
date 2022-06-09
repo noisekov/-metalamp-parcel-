2723588010cee4fac743d2ac5f194c9972671fca
Vladimir <noise123q@mail.ru>
2022-06-10 00:54:10 +0400

    add range-slider in forms-elements

--- src/components/form-elements/form-elements.js
+++ src/components/form-elements/form-elements.js
@@ -2,26 +2,44 @@ import AirDatepicker from 'air-datepicker';
 import 'air-datepicker/air-datepicker.css';
 // import 'item-quantity-dropdown';
 // import 'item-quantity-dropdown/lib/item-quantity-dropdown.css';
-// import noUiSlider from 'nouislider';
-// import * as noUiSlider from 'nouislider';
-// import '/nouislider/dist/nouislider.css';
+import noUiSlider from 'nouislider';
+import 'nouislider/dist/nouislider.css';
+
 //air-datapicker
-let cal1, cal2;
+let cal1, cal2,calendar;
 let apply = {
    content: 'Применить',
    className:'custom-button',
-   onclick: () => {
-      return true
+   onclick: (daySelect) => {
+      let date = newDate('1995-07-17');
+      daySelect.selectDate(date);
+      daySelect.setViewDate(date);
    }
 }
-cal1 = new AirDatepicker('#date-picker',{
+cal1 = new AirDatepicker('#date-picker-cal1',{
    range: true,
    multipleDatesSeparator: '-',
    inline: false,
-   buttons: ['clear', apply]
+   buttons: ['clear', apply],
+   onSelect({date}) {
+      cal1.upDate({
+         minDate: date
+      })
+   }
+});
+cal2 = new AirDatepicker('#date-picker-cal2',{
+   range: true,
+   multipleDatesSeparator: '-',
+   inline: false,
+   buttons: ['clear', apply],
+   onSelect({date}) {
+      cal2.upDate({
+         maxDate: date
+      })
+   }
 });
 //calendar
-cal2 = new AirDatepicker('#calendar',{
+calendar = new AirDatepicker('#calendar',{
    inline:true,
    buttons: ['clear', apply],
    range: true,
