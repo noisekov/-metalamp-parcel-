e34353b847f224f5051944e0b1a71f30cf8e8c99
Vladimir <noise123q@mail.ru>
2022-07-01 14:58:24 +0400

    new like-button

--- src/components/like-button/like-button.js
+++ src/components/like-button/like-button.js
@@ -1,18 +1,20 @@
-const btnLike = document.querySelector("#js-like-color");
-const likeImg = document.querySelector('.material-icons_like');
-const likeLabel = document.querySelector("#js-like-count");
+const btnLike = document.querySelectorAll(".like-button");
+const valueLike = document.querySelector(".like-button__value");
 
-btnLike.addEventListener("click", function  heart(e) {
-    if (e.target.closest("#js-like-color")){
-        likeImg.classList.remove('material-icons_like')
-        likeImg.classList.add('material-icons_like-color')
-        likeImg.innerHTML = "favorite";
-    } else{
-        likeImg.classList.remove('material-icons_like')
-        likeImg.classList.add('material-icons_like-color')
-        likeImg.innerHTML = "favorite_border";
-    } 
-
-})
+let result;
+document.addEventListener("click", heart)
+function  heart(e) {
+    if (e.target.closest("#js-like-button")){
+        e.preventDefault();
+        for (let i = 0; i < btnLike.length; i ++){
+            btnLike[i].classList.toggle(".like-button_active")
+          
+        }
+        result = +valueLike.innerHTML;
+        result++;
+        valueLike.innerHTML=""
+        valueLike.innerHTML += result
+    }  
+}
 
 
