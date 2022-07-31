b791840391c09794fb0215669280259617f9eba5
Vladimir <noise123q@mail.ru>
2022-07-31 09:48:11 +0300

    correct space

--- src/components/appartment/appartment.js
+++ src/components/appartment/appartment.js
@@ -1,30 +1,19 @@
-// document.addEventListener('click', showSlideLeft)
-// document.addEventListener('click', showSlideRight)
-
-// const slideLeft = document.querySelector("#js-slide-left");
-// const slideRight = document.querySelector("#js-slide-right");
-// const slidePhoto = document.querySelectorAll(".appartment__img");
-
-// function showSlideLeft (e) {
-//     if(e.target.closest("#js-slide-left")){
-//         for (let i = 0; i < slidePhoto.length; i++) {
-//             slidePhoto[i].style.left -= +271 + 'px';
-//         }
-//     }
-// }
-// function showSlideRight (ev) {
-//     if(ev.target.closest("#js-slide-right")){
-//         for (let i = 0; i < slidePhoto.length; i++){
-//             slidePhoto[i].style.right += +271 + 'px';
-//         }
-//     }
-// }
 const appartImg = document.querySelectorAll(".appartment__img");
 const appartDots = document.querySelectorAll(".appartment__slider-bar-dots");
 const appartBar = document.querySelector(".appartment__slider-bar");
+const appartSliderBar = document.querySelectorAll(".appartment__slider-bar");
 
-appartDots.forEach(e => {
-    e.addEventListener("click", function(e){
+if (appartSliderBar.length > 0) {
+    appartSliderBar.forEach(el => {
+        el.addEventListener("click", function(e){
+            console.log(this);
+        })
+    })
+}
+
+
+appartDots.forEach(el => {
+    el.addEventListener("click", function(e){
         if (e.target.closest(".appartment__slider-bar-dots")){
             appartDots.forEach(e => {
                 e.classList.remove("current");
