6ea6685f4b51a6123b6bcd98357b32528776360e
Vladimir <noise123q@mail.ru>
2022-08-10 19:43:54 +0300

    add button in airdatepicker

--- src/components/air-datepicker/air-datepicker.js
+++ src/components/air-datepicker/air-datepicker.js
@@ -6,46 +6,36 @@ let calInTwo, onePicker,calendarInPage;
 
 let apply = {
    content: 'Применить',
-   className:'custom-button',
-   onclick: (daySelect) => {
-      // let date = newDate('1995-07-17');
-      // daySelect.selectDate(date);
-      // daySelect.setViewDate(date);
+   className:'Add',
+   onClick: (evt) => {
+      evt.hide()
    }
 }
 
 calInTwo = new AirDatepicker('#cal-two-input',{
    range: true,
-   multipleDatesSeparator: '-',
-   inline: false,
    buttons: ['clear', apply],
-   selectedDates:[]
 });
+
 calInTwo = new AirDatepicker('#cal-two-input-find-number',{
    range: true,
    multipleDatesSeparator: '-',
-   inline: false,
    buttons: ['clear', apply],
-   selectedDates:[]
 });
 calInTwo = new AirDatepicker('#cal-two-input-choice',{
    range: true,
    multipleDatesSeparator: '-',
-   inline: false,
    buttons: ['clear', apply],
-   selectedDates:[]
 });
 
 
 onePicker = new AirDatepicker('#one-picker',{
    range: true,
    multipleDatesSeparator: ' - ',
-   inline: false,
    buttons: ['clear', apply],
 });
 
 calendarInPage = new AirDatepicker('#calendar',{
-   inline:true,
    buttons: ['clear', apply],
    range: true,
    multipleDatesSeparator: '-',
