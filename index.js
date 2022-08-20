46de63582e857eba6259a41d9845eca07d29cf17
Vladimir <noise123q@mail.ru>
2022-08-20 18:52:17 +0300

    Updates

--- index.js
+++ index.js
@@ -212,7 +212,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57582" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62996" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
@@ -389,4 +389,4 @@ function hmrAcceptRun(bundle, id) {
   }
 }
 },{}]},{},["../../../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js"], null)
-//# sourceMappingURL=/style.251b6741.js.map
+//# sourceMappingURL=/index.js.map
